416c5a6759a2af7c367f15876be5d5911e5838d5
CareyFrancis <carexfm@gmail.com>
2019-01-24 13:58:39 +0300

    Corrected some syntax errors

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -7,11 +7,19 @@ import { ProfileService } from '../../services/profile.service'
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  private profile: any;
+  private repos: any;
 
   constructor(private profileService: ProfileService) {
     this.profileService.getProfileInfo().subscribe(profile=>{
-      console.log(profile)
+      console.log(profile);
+      this.profile = profile;
     });
+    
+    this.profileService.getProfileRepo().subscribe(repos =>{
+      console.log(repos);
+      this.repos = repos;
+    })
    }
 
   ngOnInit() {
