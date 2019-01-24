0aacd2990f7372af2564f6307b76587c391323f2
CareyFrancis <carexfm@gmail.com>
2019-01-24 19:10:19 +0300

    Added FormsModule to imports in the app.module.ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
+import { FormsModule } from '@angular/forms';
 import{ ProfileService } from '../services/profile.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
+import { HighlightDirective } from './highlight.directive';
+import { DatecountPipe } from './datecount.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    HighlightDirective,
+    DatecountPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [ProfileService],
   bootstrap: [AppComponent]
